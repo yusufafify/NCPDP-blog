075303c26a36f5503d4f8f2d337288afce8c97d0
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-25 01:21:55 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-//?This JavaScript code creates a navbar element with an ID of "navbar" and adds an event 
-//?listener to the window object to detect when the user scrolls. When the user scrolls, 
-//?the code checks if the vertical scroll position is greater than zero. If it is, 
-//?the code adds a class "scrolled" to the navbar element. Otherwise, 
-//?if the scroll position is zero or less, 
+//?This JavaScript code creates a navbar element with an ID of "navbar" and adds an event
+//?listener to the window object to detect when the user scrolls. When the user scrolls,
+//?the code checks if the vertical scroll position is greater than zero. If it is,
+//?the code adds a class "scrolled" to the navbar element. Otherwise,
+//?if the scroll position is zero or less,
 //?the "scrolled" class is removed from the navbar element.
 
 // Get the navbar element by its ID
@@ -10,18 +10,12 @@ const navbar = document.getElementById("navbar");
 
 // Add a scroll event listener to the window object
 window.addEventListener("scroll", () => {
-
   // Check if the vertical scroll position is greater than 0
   if (window.scrollY > 0) {
-
     // If the scroll position is greater than 0, add the "scrolled" class to the navbar element
     navbar.classList.add("scrolled");
-
   } else {
-
     // If the scroll position is 0 or less, remove the "scrolled" class from the navbar element
     navbar.classList.remove("scrolled");
-
   }
-
 });
--- js/submit.js
+++ js/submit.js
@@ -1,12 +1,11 @@
 // Define a QuickSubscribe class to handle the subscription form behavior
 class QuickSubscribe {
-
   // Constructor for creating an instance of the QuickSubscribe class
   constructor(element) {
     this._element = element;
-    this._button = element.querySelector('.subscribe-button');
-    this._field = element.querySelector('.subscribe-field');
-    this._input = element.querySelector('#subscribe-input');
+    this._button = element.querySelector(".subscribe-button");
+    this._field = element.querySelector(".subscribe-field");
+    this._input = element.querySelector("#subscribe-input");
 
     // Call the _bindEventListeners method to add event listeners to the form elements
     this._bindEventListeners();
@@ -14,9 +13,8 @@ class QuickSubscribe {
 
   // Method to bind event listeners to the form elements
   _bindEventListeners() {
-
     // Add a submit event listener to the form element
-    this._element.addEventListener('submit', (e) => {
+    this._element.addEventListener("submit", (e) => {
       e.preventDefault();
 
       // Call the _submit method to handle form submission
@@ -24,7 +22,7 @@ class QuickSubscribe {
     });
 
     // Add a click event listener to the subscribe button
-    this._button.addEventListener('click', (e) => {
+    this._button.addEventListener("click", (e) => {
       e.preventDefault();
 
       // Call the _showField method to show the subscription field
@@ -32,46 +30,46 @@ class QuickSubscribe {
     });
 
     // Add focus and blur event listeners to the subscription input field
-    this._input.addEventListener('focus', (e) => {
-      this._inputFocused(true);  
+    this._input.addEventListener("focus", (e) => {
+      this._inputFocused(true);
     });
 
-    this._input.addEventListener('blur', (e) => {
-      this._inputFocused(false);  
+    this._input.addEventListener("blur", (e) => {
+      this._inputFocused(false);
     });
 
     // Add an input event listener to the subscription input field
-    this._input.addEventListener('input', (e) => {
-      this._inputHandleInput();  
+    this._input.addEventListener("input", (e) => {
+      this._inputHandleInput();
     });
   }
 
   // Method to show the subscription field
   _showField() {
-    this._element.classList.add('show-field');
+    this._element.classList.add("show-field");
   }
 
   // Method to hide the subscription field
   _hideField() {
-    this._element.classList.remove('show-field'); 
+    this._element.classList.remove("show-field");
   }
 
   // Method to handle form submission
   _submit() {
-    this._element.classList.add('submitted');
+    this._element.classList.add("submitted");
     this._hideField();
   }
 
   // Method to handle changes in the subscription input field focus
   _inputFocused(focused = true) {
-    this._field.classList.toggle('focused', focused);
+    this._field.classList.toggle("focused", focused);
   }
 
   // Method to handle changes in the subscription input field input
   _inputHandleInput() {
-    this._field.classList.toggle('has-value', this._input.value.length > 0);  
+    this._field.classList.toggle("has-value", this._input.value.length > 0);
   }
 }
 
 // Create a new instance of the QuickSubscribe class with a specific element passed as an argument
-new QuickSubscribe(document.querySelector('.quick-subscribe'));
+new QuickSubscribe(document.querySelector(".quick-subscribe"));
